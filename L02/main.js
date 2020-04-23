f8c4a71ec5110b1346a41cb02b51453601e72179
DesireeSchmidlin <45148361+DesiS96@users.noreply.github.com>
2020-04-23 14:06:54 +0200

    Add files via upload

---
+++ L02/main.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('%c Hello Word!', 'font-family: Arial; font-weight: bold; font-size: 30px; color:blue');
+//# sourceMappingURL=main.js.map
---
+++ L02/main.ts
@@ -0,0 +1,1 @@
+console.log('%c Hello Word!', 'font-family: Arial; font-weight: bold; font-size: 30px; color:blue');
