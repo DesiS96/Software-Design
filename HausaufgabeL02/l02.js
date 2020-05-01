6314772af803d26a4cc598dc8c698f96c33f3775
DesiS96 <desischmi@gmx.de>
2020-05-01 15:17:55 +0200

    hausaufgabe021

---
+++ HausaufgabeL02/l02.js
@@ -0,0 +1,14 @@
+"use strict";
+let i = 42;
+let pi = 3.1415;
+let salute = "Hello,World";
+var i2 = 42;
+var pi2 = 3.1415;
+var salute2 = "Hello,World";
+console.log("Zahlen mit Typen");
+console.log(i, pi, salute);
+console.log("Zahlen ohne Typen");
+console.log(i2, pi2, salute2);
+let ia = [10];
+console.log(ia);
+//# sourceMappingURL=l02.js.map
---
+++ HausaufgabeL02/l02.ts
@@ -0,0 +1,16 @@
+let i: number = 42;
+let pi: number = 3.1415;
+let salute: string = "Hello,World";
+
+var i2 = 42;
+var pi2 = 3.1415;
+var salute2 = "Hello,World";
+
+console.log("Zahlen mit Typen");
+console.log(i, pi, salute);
+
+console.log("Zahlen ohne Typen");
+console.log(i2, pi2, salute2);
+
+let ia = new Array<number>(10);
+let ca = new Array<string>(30);
