33baa46eeb86b16d82abbca57b9ec6d385747909
DesiS96 <desischmi@gmx.de>
2020-05-06 15:37:56 +0200

    poem2

---
+++ RandomPoems/main.js
@@ -0,0 +1,61 @@
+"use strict";
+var randompoem;
+(function (randompoem) {
+    let subjects = ["Light", "L", "Near", "Misa", "Mello", "Ryuk"];
+    let verbs = ["siegt über", "liebt", "isst", "hasst", "benutzt", "hintergeht"];
+    let objects = ["das Death Note", "L", "Light", "Äpfel", "Kira", "die Shinigami"];
+    function getShortestLength(subjects, verbs, objects) {
+        if (subjects.length <= verbs.length) {
+            if (objects.length < subjects.length) {
+                return objects.length;
+            }
+            else {
+                if (subjects.length <= objects.length) {
+                    return subjects.length;
+                }
+                else {
+                    return objects.length;
+                }
+            }
+        }
+        else {
+            if (verbs.length < objects.length) {
+                return verbs.length;
+            }
+            else {
+                return objects.length;
+            }
+        }
+    }
+    let length = getShortestLength(subjects, verbs, objects);
+    let index;
+    let i = 0;
+    for (i = 0; i < 3; i++) {
+        let j = 0;
+        for (; j < length; j++) {
+            let indexTemp;
+            while (true) {
+                indexTemp = Math.floor(Math.random() * index[i].length);
+                let contain = false;
+                for (let k = 0; k < index[i].length; k++) {
+                    if (index[i][k] == indexTemp) {
+                        contain = true;
+                    }
+                    else {
+                        contain = false;
+                    }
+                }
+                if (contain == false) {
+                    break;
+                }
+            }
+            indexTemp = index[i][j];
+        }
+    }
+    i = 0;
+    while (i < length) {
+        console.log(subjects[index[1][i]] + "" + verbs[index[2][i]] + "" + objects[index[3][i]]);
+        i = i + 1;
+    }
+})(randompoem || (randompoem = {}));
+//# sourceMappingURL=main.js.map
---
+++ RandomPoems/main.ts
@@ -0,0 +1,88 @@
+namespace randompoem {
+
+let subjects: string[] = ["Light", "L", "Near", "Misa", "Mello", "Ryuk"];
+let verbs: string[] = ["siegt über", "liebt", "isst", "hasst", "benutzt", "hintergeht"];
+let objects: string[] = ["das Death Note", "L", "Light", "Äpfel", "Kira", "die Shinigami"];
+
+function getShortestLength (subjects: string[], verbs: String[], objects: string[]): number {
+
+
+    if (subjects.length <= verbs.length) {
+        if (objects.length < subjects.length) {
+             return objects.length;
+
+        }
+        else {
+         if (subjects.length <= objects.length) {
+             return subjects.length;
+
+         } else {
+
+            return objects.length;
+         }
+        
+        }
+     }
+    
+    else {
+            if (verbs.length < objects.length) {
+                return verbs.length;
+            }
+            else {
+                return objects.length;
+    
+            }
+        }       
+    
+}
+
+let length: number = getShortestLength(subjects, verbs, objects);
+let index: number[][];
+let i: number = 0;
+
+for (i = 0; i < 3; i++) {
+    let j: number = 0;
+
+    for (; j < length; j++) {
+        let indexTemp: number;
+
+        while (true) {
+
+            indexTemp = Math.floor(Math.random() * index[i].length);
+
+            let contain: boolean = false;
+
+            for (let k: number = 0; k < index[i].length; k++) {
+
+                    if (index[i][k] == indexTemp) {
+                        contain = true;
+                    }
+                    else {
+                        contain = false;
+                    } 
+                }   
+
+            if (contain == false) {
+                    break;
+            }
+        }
+
+        indexTemp = index[i][j];
+
+          
+    }
+
+}
+
+i = 0;
+
+while (i < length) {
+
+    console.log(subjects[index[1][i]] + "" + verbs[index[2][i]] + "" + objects[index[3][i]]);
+
+    i = i + 1;
+
+
+}
+
+}
