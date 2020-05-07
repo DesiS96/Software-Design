b23bb7cda5d4b3d95f309dd32029cc5a4cc1b504
DesiS96 <desischmi@gmx.de>
2020-05-07 21:21:00 +0200

    l03_2

--- L03/main.js
+++ L03/main.js
@@ -32,7 +32,35 @@ var l03;
             this.firstname = firstname;
             this.lastname = lastname;
             this.age = age;
-            //public override string ToString() { /* TODO */ }
+        }
+        //public override string ToString() { /* TODO */ }
+        toString() {
+            return this.firstname + " " + this.lastname + " " + this.age;
+        }
+    }
+    let alice = new Person("Alice", "Doe", 22);
+    console.log(alice.toString());
+    /*function getPerson(firstname: string, lastname: string, age: number): Person {
+
+        let name: Person = new Person(firstname, lastname, age);
+
+        return name;
+
+    }
+
+    let heather = getPerson("Heather", "Mason", 16);*/
+    let piet = new Person("Piet", "Miller", 33);
+    let rene = new Person("Rene", "Jackson", 24);
+    let tom = new Person("Tom", "Schmitt", 18);
+    let yuki = new Person("Yuki", "Miyazaki", 45);
+    let caroline = new Person("Caroline", "Amber", 12);
+    let arr = [piet, rene, tom, yuki, caroline];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].age >= 20) {
+            console.log(arr[i].toString());
+        }
+        else {
+            console.log("Person ist unter 20");
         }
     }
 })(l03 || (l03 = {}));
--- L03/main.ts
+++ L03/main.ts
@@ -34,18 +34,62 @@ namespace l03 {
         public lastname: string;
         public age: number;
 
-        constructor(firstname: string, lastname: string, age: number){
-
+        constructor(firstname: string, lastname: string, age: number) {
+            
             this.firstname = firstname;
             this.lastname = lastname;
             this.age = age;
+
+        }
+
         //public override string ToString() { /* TODO */ }
+
+        toString(): string {
+
+            return this.firstname + " " + this.lastname + " " + this.age;
+
+        }
+    }
+
+    let alice: Person = new Person("Alice", "Doe", 22);
+
+    console.log(alice.toString());
+
+    
+
+    /*function getPerson(firstname: string, lastname: string, age: number): Person {
+
+        let name: Person = new Person(firstname, lastname, age);
+
+        return name;
+
+    }
+
+    let heather = getPerson("Heather", "Mason", 16);*/
+    
+    let piet: Person = new Person("Piet", "Miller", 33);
+    let rene: Person = new Person("Rene", "Jackson", 24);
+    let tom: Person = new Person("Tom", "Schmitt", 18);
+    let yuki: Person = new Person("Yuki", "Miyazaki", 45);
+    let caroline: Person = new Person("Caroline", "Amber", 12);
+
+    let arr: Person[] = [piet, rene, tom, yuki, caroline];
+   
+    for (let i: number = 0; i < arr.length; i++) {
+
+        if (arr[i].age >= 20) {
+
+            console.log(arr[i].toString());
+        }
+        else {
+
+            console.log("Person ist unter 20");
+        }
+
 
     }
 
 
 
-
 }
 
-}
