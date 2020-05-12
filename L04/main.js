fe7c6e2ea522cd9965907d0d5b92e3011eb9bb10
DesiS96 <desischmi@gmx.de>
2020-05-12 11:45:39 +0200

    uebunginvo

---
+++ L04/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var l04;
+(function (l04) {
+    console.log("Dies ist eine Log-Ausgabe");
+    console.error("Fehler!");
+    console.info("Dies ist eine Info-Ausgabe");
+    console.warn("Das ist eine Warnung!");
+})(l04 || (l04 = {}));
+//# sourceMappingURL=main.js.map
---
+++ L04/main.ts
@@ -0,0 +1,11 @@
+namespace l04 {
+    
+    console.log("Dies ist eine Log-Ausgabe");
+    console.error("Fehler!");
+    console.info("Dies ist eine Info-Ausgabe");
+    console.warn("Das ist eine Warnung!");
+
+
+
+
+}
