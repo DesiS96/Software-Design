f6a3d889a5a2d5962fdb235cdcb05b5e50e9a69b
DesiS96 <desischmi@gmx.de>
2020-05-13 14:43:00 +0200

    zelda

---
+++ L05/main.js
@@ -0,0 +1,11 @@
+"use strict";
+var L05;
+(function (L05) {
+    class Person {
+    }
+    let zelda = new Person();
+    zelda.age = 17;
+    zelda.name = "Zelda";
+    console.log(zelda.name + ", " + zelda.age);
+})(L05 || (L05 = {}));
+//# sourceMappingURL=main.js.map
---
+++ L05/main.ts
@@ -0,0 +1,25 @@
+namespace L05 {
+
+    class Person {
+
+        public name: string;
+        public age: number;
+
+
+
+    }
+
+    let zelda: Person = new Person();
+
+    zelda.age = 17;
+    zelda.name = "Zelda";
+
+    console.log(zelda.name + ", " + zelda.age);
+
+
+
+
+
+
+
+}
