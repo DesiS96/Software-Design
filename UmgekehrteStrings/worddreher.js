0a1e78cf58af2a52953cded8bbfe9a39c67ffb3d
DesiS96 <desischmi@gmx.de>
2020-05-13 10:49:17 +0200

    updatelauteequest

--- UmgekehrteStrings/worddreher.js
+++ UmgekehrteStrings/worddreher.js
@@ -3,26 +3,39 @@ var worddreher;
 (function (worddreher) {
     let scentence = "this scentence is twisted"; //müsste eigentlich selber eigegeben werden
     console.log(scentence);
+    //there is an typo in the parameter(sentence), use for parameters "_" at the begining.
     function twistCharacters(scentence) {
+        // you can conclude line 9 and line 10 so the naming makes more sense and is better readable.
         let twistedString = scentence.split("");
         twistedString.reverse();
+        //  -> move initialization of the index to the loop
         let i;
+        // dont know if the line breaks are for the comments, if not they are overused.
         let twistedWords = "";
         for (i = 0; i <= twistedString.length; i++) {
             twistedWords += twistedString[i];
         }
+        /* you can conclude line 22 and line 23 so the naming makes more sense (name says string but is initialized as array)
+         and is better readable.*/
         let twistedStringArr = twistedWords.split(" ");
         twistedStringArr.reverse();
+        // directly return the value, also dont call the variable same name with numeration
         let twistedString2 = twistedStringArr.join(" ");
         return twistedString2;
     }
     function twistWords(scentence) {
+        /* you can conclude line 22 and line 23 so the naming makes more sense (name says string but is initialized as array)
+         and is better readable.*/
         let twistedArray = scentence.split(" "); //String als einzelne Wörter
-        twistedArray.reverse();
+        /*  twistedArray.reverse();
+          directly return the value*/
         let twistedString = twistedArray.join(" ");
         return twistedString; //detsiwt si ecnetnecs siht (unfertig)
     }
     function twistCharacterAndWords(scentence) {
+        /*  you dont need to initialize the func calls into an variable
+          you could just do something like --> return twistWords(twistCharacters(scentence))
+          then line 49, 51 and 53 is obsolete*/
         let twistedString = twistCharacters(scentence);
         let twistedString2 = twistWords(twistedString);
         return twistedString2;
--- UmgekehrteStrings/worddreher.ts
+++ UmgekehrteStrings/worddreher.ts
@@ -3,14 +3,14 @@ namespace worddreher {
     let scentence: string = "this scentence is twisted"; //müsste eigentlich selber eigegeben werden
     console.log(scentence);
 
-    there is an typo in the parameter(sentence), use for parameters "_" at the begining.
+    //there is an typo in the parameter(sentence), use for parameters "_" at the begining.
     function twistCharacters(scentence: string): string {
-        you can conclude line 9 and line 10 so the naming makes more sense and is better readable.
+       // you can conclude line 9 and line 10 so the naming makes more sense and is better readable.
         let twistedString: string[] = scentence.split("");
         twistedString.reverse();
-         -> move initialization of the index to the loop
+       //  -> move initialization of the index to the loop
         let i: number;
-        dont know if the line breaks are for the comments, if not they are overused.
+       // dont know if the line breaks are for the comments, if not they are overused.
         let twistedWords: string = "";
         
         for (i = 0; i <= twistedString.length; i++) {
@@ -18,12 +18,12 @@ namespace worddreher {
             twistedWords += twistedString[i];
 
         }
-        you can conclude line 22 and line 23 so the naming makes more sense (name says string but is initialized as array)
-        and is better readable.
+       /* you can conclude line 22 and line 23 so the naming makes more sense (name says string but is initialized as array)
+        and is better readable.*/
         let twistedStringArr: string[] = twistedWords.split(" ");
 
         twistedStringArr.reverse();
-        directly return the value, also dont call the variable same name with numeration
+      // directly return the value, also dont call the variable same name with numeration
         let twistedString2: string = twistedStringArr.join(" ");
         
         return twistedString2;
@@ -31,21 +31,21 @@ namespace worddreher {
     }
 
     function twistWords(scentence: string): string {
-        you can conclude line 22 and line 23 so the naming makes more sense (name says string but is initialized as array)
-        and is better readable.
+       /* you can conclude line 22 and line 23 so the naming makes more sense (name says string but is initialized as array)
+        and is better readable.*/
         let twistedArray: string[] = scentence.split(" "); //String als einzelne Wörter
         
-        twistedArray.reverse();
-        directly return the value
+      /*  twistedArray.reverse();
+        directly return the value*/
         let twistedString: string = twistedArray.join(" ");
 
         return twistedString; //detsiwt si ecnetnecs siht (unfertig)
     }
 
     function twistCharacterAndWords(scentence: string): string {
-        you dont need to initialize the func calls into an variable
+      /*  you dont need to initialize the func calls into an variable
         you could just do something like --> return twistWords(twistCharacters(scentence))
-        then line 49, 51 and 53 is obsolete
+        then line 49, 51 and 53 is obsolete*/
         let twistedString: string = twistCharacters(scentence);
 
         let twistedString2: string = twistWords(twistedString);
