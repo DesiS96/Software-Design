d2937251eff0e2c211d63f9315d51312b0fea186
DesiS96 <desischmi@gmx.de>
2020-06-23 10:36:30 +0200

    main

--- GenerischerBaum/main.js
+++ GenerischerBaum/main.js
@@ -1,25 +1,23 @@
-"use strict";
 var GenerischerBaum;
 (function (GenerischerBaum) {
     //let nodes: string[] = [];
-    let tree = new GenerischerBaum.Tree([]);
-    let root = tree.createNode("root");
-    let child1 = tree.createNode("child1");
+    var tree = new Tree([]);
+    var root = tree.createNode("root");
+    var child1 = tree.createNode("child1");
     console.log(child1);
-    let child2 = tree.createNode("child2");
+    var child2 = tree.createNode("child2");
     //let node = new TreeNode<string>("node", []);
     root.appendChild(child1);
     root.appendChild(child2);
-    let grand11 = tree.createNode("grand11");
-    let grand12 = tree.createNode("grand12");
-    let grand13 = tree.createNode("grand13");
+    var grand11 = tree.createNode("grand11");
+    var grand12 = tree.createNode("grand12");
+    var grand13 = tree.createNode("grand13");
     child1.appendChild(grand11);
     child1.appendChild(grand12);
     child1.appendChild(grand13);
-    let grand21 = tree.createNode("grand21");
+    var grand21 = tree.createNode("grand21");
     child2.appendChild(grand21);
     console.log(child2.children);
     child1.removeChild(grand12);
     root.printTree();
 })(GenerischerBaum || (GenerischerBaum = {}));
-//# sourceMappingURL=main.js.map
