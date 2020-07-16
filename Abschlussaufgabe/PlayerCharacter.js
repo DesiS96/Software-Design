f10fb582890232060501836d4a0a93831daad7ea
DesiS96 <desischmi@gmx.de>
2020-07-16 13:02:31 +0200

    inventory_commands_drop_method

--- Abschlussaufgabe/PlayerCharacter.js
+++ Abschlussaufgabe/PlayerCharacter.js
@@ -2,10 +2,32 @@
 var Abschlussaufgabe;
 (function (Abschlussaufgabe) {
     class PlayerCharacter extends Abschlussaufgabe.Character {
-        constructor(_name, _life, _inventory, _attack) {
+        constructor(_name, _life, _inventory, _attack, _commands) {
             super(_name, _life);
             this.inventory = _inventory;
             this.attack = _attack;
+            this.commands = _commands;
+        }
+        drop() {
+            let userprompt = window.prompt("Which item do you want to drop?:"); //soll später auf Konsole eingegeben werden können
+            for (let i; i < this.inventory.length; i++) {
+                /*if (this.inventory[i].name == userprompt) {
+                    console.log("You decided to drop " + this.inventory[i].name);
+                }else{*/
+                if (i == this.inventory.length && this.inventory[i].name != userprompt) {
+                    console.log("Sorry but there'S no such item in your inventoy");
+                }
+            }
+        }
+        showInventory() {
+            console.log("Your inventory contains:");
+            for (let i; i < this.inventory.length; i++) {
+                console.log("a" + this.inventory[i]);
+            }
+        }
+        showCommands() {
+            let commandsString = this.commands.toString();
+            console.log(commandsString);
         }
     }
     Abschlussaufgabe.PlayerCharacter = PlayerCharacter;
--- Abschlussaufgabe/PlayerCharacter.ts
+++ Abschlussaufgabe/PlayerCharacter.ts
@@ -4,12 +4,48 @@ namespace Abschlussaufgabe {
 
         public inventory: Item[];
         public attack: number;
+        public commands: string[];
 
-        constructor(_name: string, _life: number, _inventory: Item[], _attack: number) {
+        constructor(_name: string, _life: number, _inventory: Item[], _attack: number, _commands: string[]) {
 
             super(_name, _life);
             this.inventory = _inventory;
             this.attack = _attack;
+            this.commands = _commands;
+        }
+
+        drop(): void {
+
+            let userprompt: string = window.prompt("Which item do you want to drop?:" ); //soll später auf Konsole eingegeben werden können
+
+            for (let i: number; i < this.inventory.length; i++) {
+
+                /*if (this.inventory[i].name == userprompt) {
+                    console.log("You decided to drop " + this.inventory[i].name);
+                }else{*/
+
+                    if (i == this.inventory.length && this.inventory[i].name != userprompt) {
+
+                        console.log("Sorry but there'S no such item in your inventoy");
+                    }
+            }
+        }
+
+        showInventory(): void {
+
+            console.log("Your inventory contains:");
+
+            for (let i: number; i < this.inventory.length; i++) {
+                console.log("a" + this.inventory[i]);
+
+            }
+        }
+
+        showCommands(): void {
+
+            let commandsString: string = this.commands.toString();
+
+            console.log(commandsString);
         }
 
     }
