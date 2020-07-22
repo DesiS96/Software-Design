523fc542abca23e2b66c894c99ee0f7cbefdca69
DesiS96 <desischmi@gmx.de>
2020-07-22 19:22:48 +0200

    html_konsole

--- Abschlussaufgabe/main.js
+++ Abschlussaufgabe/main.js
@@ -1,6 +1,26 @@
 "use strict";
 var Abschlussaufgabe;
 (function (Abschlussaufgabe) {
-    console.log("Hello");
+    const text = document.createElement("div");
+    document.body.appendChild(text);
+    //const inputfield = document.createElement("input");
+    //document.body.appendChild(inputfield);
+    text.innerHTML = "Welcome to the game. What do you want to do?<br>>start game (s)<br>>quit game (q)";
+    //inputfield
+    let userInput = window.prompt("Select either s to start the game or q to quit it:");
+    text.innerHTML = text.innerHTML + "<br>" + userInput;
+    switch (userInput) {
+        case "s": {
+            text.innerHTML = text.innerHTML + "<br>" + "You decide to enter the forgotten Mines <br> All you have is a torch to lighten the way and your magic to attack foes. <br> You see ";
+            break;
+        }
+        case "q": {
+            text.innerHTML = text.innerHTML + "<br>" + "You've ended the game. <br> Hope to see you again soon!";
+            break;
+        }
+        default: {
+            text.innerHTML = text.innerHTML + "<br>" + "Please restart Page and enter s or q.";
+        }
+    }
 })(Abschlussaufgabe || (Abschlussaufgabe = {}));
 //# sourceMappingURL=main.js.map
